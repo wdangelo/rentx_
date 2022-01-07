446f87c9a117e305e8d5c0723c13b3e2d28b02e6
wdangelo <wdangelo1983@gmail.com>
2022-01-07 15:23:35 -0300

    criando list de carros

---
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -0,0 +1,47 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-momory/CarsRepositoryInMemory";
+
+import { ListCarsUseCase } from "./ListCarsUseCase";
+
+let listCarsUseCase: ListCarsUseCase;
+let carsRespositoryInMemory: CarsRepositoryInMemory;
+
+describe("List Cars", () => {
+  beforeEach(() => {
+    carsRespositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRespositoryInMemory);
+  });
+
+  it("should be able to list all available cars", async () => {
+    const car = await carsRespositoryInMemory.create({
+      name: "Car1",
+      description: "Car description",
+      daily_rate: 110.0,
+      license_plate: "DEF-1234",
+      fine_amount: 40,
+      brand: "Car_brand",
+      category_id: "038b4af6-a0b6-4e42-89cf-2b892c6aaf83",
+    });
+
+    const cars = await listCarsUseCase.execute({});
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("shold be able to list all available cars by name", async () => {
+    const car = await carsRespositoryInMemory.create({
+      name: "Car2",
+      description: "Car description",
+      daily_rate: 110.0,
+      license_plate: "DEF-1234",
+      fine_amount: 40,
+      brand: "Car_brand_test",
+      category_id: "038b4af6-a0b6-4e42-89cf-2b892c6aaf83",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      brand: "Car_brand",
+    });
+    console.log(cars);
+    expect(cars).toEqual([car]);
+  });
+});
