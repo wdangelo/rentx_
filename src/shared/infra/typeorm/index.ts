e55b70d809a1176389f225e6600262fa2dddadac
wdangelo <wdangelo1983@gmail.com>
2022-05-11 10:47:41 -0300

    teste unitario supertest

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,7 +4,9 @@ export default async (host = "database"): Promise<Connection> => {
   const defaultOption = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOption, {
-      host,
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test" ? "rentx_test" : defaultOption.database,
     })
   );
 };
